a56e67e70cd0d730421f8d15654f67d359e18066
Leandro Pilz <leandro.pilzz@gmail.com>
2019-05-06 19:06:34 -0300

    Finalizado cap. 60 - Validator textformfield.

--- lib/pages/login_pages.dart
+++ lib/pages/login_pages.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  final _tLoginController = TextEditingController();
-  final _tPasswController = TextEditingController();
+  final _tLoginController =
+      TextEditingController(text: "leandro.pilzz@gmail.com");
+  final _tPasswController = TextEditingController(text: "12345678");
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -12,20 +15,38 @@ class LoginPage extends StatelessWidget {
       ), //appBar
       body: Padding(
         padding: EdgeInsets.all(16),
-        child: _body(),
+        child: _body(context),
       ), // Padding
     );
   }
 
-  _body() {
+  String _validatorLogin(String text) {
+    if (text.isEmpty) {
+      return "Informe o login";
+    }
+    return null;
+  }
+
+  String _validatorPasswd(String text) {
+    if (text.isEmpty) {
+      return "Informe a senha";
+    } else if (text.length < 2) {
+      return "Senha precisa ter mais de dois dígitos";
+    }
+    return null;
+  }
+
+  _body(BuildContext context) {
     return Form(
+      key: _formKey,
       child: ListView(
         children: <Widget>[
           TextFormField(
             controller: _tLoginController,
-            keyboardType: TextInputType.text,
+            validator: _validatorLogin,
+            keyboardType: TextInputType.emailAddress,
             style: TextStyle(color: Colors.black, fontSize: 16),
-            textCapitalization: TextCapitalization.characters,
+//            textCapitalization: TextCapitalization.characters,
             decoration: InputDecoration(
               labelText: "Login",
               labelStyle: TextStyle(color: Colors.black87, fontSize: 14),
@@ -41,6 +62,7 @@ class LoginPage extends StatelessWidget {
             margin: EdgeInsets.only(top: 16),
             child: TextFormField(
               controller: _tPasswController,
+              validator: _validatorPasswd,
               obscureText: true,
               keyboardType: TextInputType.number,
               style: TextStyle(color: Colors.black, fontSize: 16),
@@ -61,7 +83,9 @@ class LoginPage extends StatelessWidget {
             height: 50,
             margin: EdgeInsets.only(top: 20),
             child: RaisedButton(
-              onPressed: _onClickLogin,
+              onPressed: () {
+                _onClickLogin(context);
+              },
               color: Colors.blue,
               child: Text(
                 "Entrar",
@@ -77,9 +101,14 @@ class LoginPage extends StatelessWidget {
     ); //Form
   }
 
-  _onClickLogin() {
+  _onClickLogin(BuildContext context) {
     final login = _tLoginController;
     final passw = _tPasswController;
+
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+
     print("Login ${login.text}, senha ${passw.text}");
   }
 }
